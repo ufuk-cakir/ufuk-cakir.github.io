87b55472b0d298f69f896fd3181254bb85217a0f
Ufuk Çakır <ufukcakir2001@gmail.com>
2024-11-23 17:28:46 +0000

    update paths

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  assetPrefix: '/',
+  basePath: '/ufuk-cakir.github.io', // Replace with your GitHub repository name
+  assetPrefix: '/ufuk-cakir.github.io', 
 }
 
 module.exports = nextConfig
