835a56bc0a6f4db09df4768c048993e5d9c09bee
Ufuk Çakır <ufukcakir2001@gmail.com>
2025-04-25 22:25:37 +0200

    update config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: '/ufuk-cakir.github.io', // Replace with your GitHub repository name
+  assetPrefix: '/ufuk-cakir.github.io',
   images: {
     domains: ["avatars.githubusercontent.com"],
     // Next 13+
