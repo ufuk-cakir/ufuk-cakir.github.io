825e279efe9d539d873bf9e6b93789bb3e58815c
Ufuk Çakır <ufukcakir2001@gmail.com>
2025-04-27 20:20:25 +0200

    update pic

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,24 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import 'highlight.js/styles/github-dark.css'; // Example: Dark theme
+import Head from "next/head";
 // or
 const App = ({ Component, pageProps }) => {
   return (
+    
+
+    <>
+
+    <Head>
+      <title>Ufuk Çakır</title>
+      <link rel="icon" href="/images/letter-u.png"/>
+    </Head>
+
     <ThemeProvider>
       <Component {...pageProps} />
     </ThemeProvider>
+
+    </>
   );
 };
 
