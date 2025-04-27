53ff0b3ed07be63532bc07f355d7e3c8d6b4f4c6
Ufuk Çakır <ufukcakir2001@gmail.com>
2025-04-27 23:03:43 +0200

    fix client side bug

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+swcMinify: true,
   reactStrictMode: true,
   images: {
 domains: ["avatars.githubusercontent.com", "images.unsplash.com"],
